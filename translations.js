48cdb327d72f4e2e3ca447c128e4add758ff1b3b
cataseven <52464808+cataseven@users.noreply.github.com>
2026-07-30 15:27:57 +0300

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,4 +1,4 @@
-export const T_VERSION = "3.32";
+export const T_VERSION = "3.33";
 
 export const LANG_NAMES = Object.freeze({
   en: "English", cs: "Čeština", da: "Dansk", de: "Deutsch", es: "Español",
